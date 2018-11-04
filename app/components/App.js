2cd3c48ac749cbbbf07db659d4276c578921292a
Carrie Scott <carriescott@Carries-MacBook-Pro.local>
2018-11-04 15:21:07 -0600

    built nav, home, and battle components. made update to webpack.config

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,24 @@
 var React = require('react');
 var Popular = require('./Popular');
+const ReactRouter = require ('react-router-dom');
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+const Nav = require('./Nav')
+const Home = require ('./Home')
+const Battle = require ('./Battle')
 
 class App extends React.Component {
   render() {
     return (
-      <div className = 'container'>
-        <Popular />
-      </div>
+      <Router>
+        <div className = 'container'>
+          <Nav />
+          <Route exact path = '/' component={Home} />
+          <Route path='/popular' component= {Popular} />
+          <Route path= '/battle' component = {Battle} />
+        </div>
+
+      </Router>
     )
   }
 }
